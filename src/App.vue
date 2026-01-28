f58b1f272c65f78e13d0390a8c6a2fcae5003cb5
Jack Chang <jackchang@reallusion.com>
2026-01-28 16:32:41 +0800

    無論什麼狀態下都可以下一題

--- src/App.vue
+++ src/App.vue
@@ -458,7 +458,7 @@ onMounted(async () => {
 							</button>
 							<button
 								class="rounded-full border border-slate-700 px-6 py-2 text-sm text-slate-200 hover:bg-slate-800 disabled:cursor-not-allowed disabled:bg-slate-600 disabled:text-slate-900"
-								:disabled="roundState !== 'revealing'" @click="nextQuestion">
+								:disabled="roundState === 'answering'" @click="nextQuestion">
 								下一題
 							</button>
 						</div>
